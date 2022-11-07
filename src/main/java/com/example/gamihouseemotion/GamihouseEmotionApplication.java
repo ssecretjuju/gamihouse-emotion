0d6ce8885a83abcbe5b0f3822b826b31077305da
gomdoricha <chayoungjun5781@gmail.com>
2022-11-07 10:40:08 +0900

    Initial Commit

---
+++ src/main/java/com/example/gamihouseemotion/GamihouseEmotionApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamihouseemotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamihouseEmotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamihouseEmotionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamihouseemotion/GamihouseEmotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamihouseemotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamihouseEmotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
